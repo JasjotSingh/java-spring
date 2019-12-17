9c5a358836d3c5f0f253211d246ffde8b5eba6f5
jasjotsingh <jasjotsingh@172.17.101.237>
2019-12-17 14:53:00 -0800

    delete comment test

--- HelloWeb/src/org/test/helloworld/helloworld.java
+++ HelloWeb/src/org/test/helloworld/helloworld.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class helloworld
+ * Servlet implementation class helloworld. comment deleted.
  */
 @WebServlet("/helloworld")
 public class helloworld extends HttpServlet {
